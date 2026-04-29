9c33baaac0f0b74fe26b05a940e9054c608faa0d
MH-liton-360 <liton41126nk@gmail.com>
2026-04-30 01:46:39 +0600

    almost okey

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
-// MongoDB URI
+// MongoDB URI.....................................!
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uru7rsz.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
