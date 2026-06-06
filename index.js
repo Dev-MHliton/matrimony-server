beae1196daffc122407d15c9b898c532c00c050f
Dev-MHliton <liton41126nk@gmail.com>
2026-06-06 06:10:55 +0600

    comment add

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config()
+
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
 
+// password and usercode 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uru7rsz.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
